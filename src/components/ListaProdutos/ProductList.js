9c27f40e6af95ec8d855027818f2fd6df9b12220
AkimGomes <ak-gomes@hotmail.com>
2023-09-14 22:22:10 -0300

    feat(productlist) - Adicionando campo de busca no componente de lista de produtos

--- src/components/ListaProdutos/ProductList.js
+++ src/components/ListaProdutos/ProductList.js
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 const ProductList = ({ token }) => {
   const navigate = useNavigate();
   const [products, setProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const fetchProducts = async () => {
     try {
@@ -50,15 +51,52 @@ const ProductList = ({ token }) => {
     navigate('/login'); 
   };
 
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/produtos/buscar/?buscar=${searchTerm}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setProducts(data);
+      } else {
+        console.error('Erro ao buscar produtos');
+      }
+    } catch (error) {
+      console.error('Erro ao buscar produtos:', error);
+    }
+  };
+
   return (
     <div>
       <div>
         <button className="botao-deslogar" onClick={handleLogout}>
-          <img src="/deslogar.png"/>
+          <img src="/deslogar.png" alt="Deslogar" />
         </button>
       </div>
       <div className='container'>
         <h2 className='listagem-heading'>Lista de Produtos</h2>
+        <form onSubmit={handleSearch}>
+        <div className='row'>
+          <div className='search-input-container'>
+            <input
+              type="text"
+              className='search-input'
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              maxLength={24}
+              placeholder="Digite o nome do produto..."
+            />
+            <button className='botao-buscar' type="submit">
+              <img src='buscar.png' alt='Buscar' className='botao-buscar' />
+            </button>
+          </div>
+        </div>
+        </form>
         <ul className="product-list">
           <li className="product-header">
             <div className="row">
@@ -85,7 +123,7 @@ const ProductList = ({ token }) => {
                     <img src="/excluir.png" alt="Excluir" className="botao-excluir" />
                   </button>
                 </div>
-                </div>
+              </div>
             </li>
           ))}
         </ul>
@@ -98,7 +136,3 @@ const ProductList = ({ token }) => {
 };
 
 export default ProductList;
-
-
-
-
